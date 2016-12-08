2eb2043e31d0b5d4278336b5f6c443801b7e90fc
Bruce Paul <brucewpaul@gmail.com>
2016-12-07 19:00:02 -0800

    uncomment data

--- server/data/baseballData.js
+++ server/data/baseballData.js
@@ -16,52 +16,52 @@ module.exports = [
       rbi: 91
     }
   },
-  // {Joey_Votto: {
-  //     hr: 29,
-  //     sb: 8,
-  //     avg: 326,
-  //     hits: 181,
-  //     rbi: 97
-  //   }
-  // },
-  // {Paul_Goldschmidt: {
-  //     hr: 24,
-  //     sb: 32,
-  //     avg: 297,
-  //     hits: 172,
-  //     rbi: 95
-  //   }
-  // },
-  // {David_Ortiz: {
-  //     hr: 28,
-  //     sb: 2,
-  //     avg: 315,
-  //     hits: 169,
-  //     rbi: 127
-  //   }
-  // },
-  // {Nolan_Arenado: {
-  //     hr: 41,
-  //     sb: 2,
-  //     avg: 294,
-  //     hits: 182,
-  //     rbi: 133
-  //   }
-  // },
-  // {Jose_Altuve: {
-  //     hr: 24,
-  //     sb: 30,
-  //     avg: 338,
-  //     hits: 216,
-  //     rbi: 96
-  //   }
-  // },
-  // {Mookie_Betts: {
-  //     hr: 31,
-  //     sb: 26,
-  //     avg: 318,
-  //     hits: 214,
-  //     rbi: 113
-  //   }
-  // }
+  {Joey_Votto: {
+      hr: 29,
+      sb: 8,
+      avg: 326,
+      hits: 181,
+      rbi: 97
+    }
+  },
+  {Paul_Goldschmidt: {
+      hr: 24,
+      sb: 32,
+      avg: 297,
+      hits: 172,
+      rbi: 95
+    }
+  },
+  {David_Ortiz: {
+      hr: 28,
+      sb: 2,
+      avg: 315,
+      hits: 169,
+      rbi: 127
+    }
+  },
+  {Nolan_Arenado: {
+      hr: 41,
+      sb: 2,
+      avg: 294,
+      hits: 182,
+      rbi: 133
+    }
+  },
+  {Jose_Altuve: {
+      hr: 24,
+      sb: 30,
+      avg: 338,
+      hits: 216,
+      rbi: 96
+    }
+  },
+  {Mookie_Betts: {
+      hr: 31,
+      sb: 26,
+      avg: 318,
+      hits: 214,
+      rbi: 113
+    }
+  }
 ]
