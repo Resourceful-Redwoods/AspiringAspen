dd3b3f65a36f91942f5152c91c553db40cb21961
Harry Bellenie <harrybellenie@gmail.com>
2016-12-12 12:16:40 -0800

    more styling

--- client/app/components/Card.jsx
+++ client/app/components/Card.jsx
@@ -7,17 +7,18 @@ const Card = (props) => {
   return (
 
     <div className='cardFront hoverable center-block' onClick={() => props.selectCard(props.card)}>
-      <img src={props.card.imageUrl}></img>
-      <div className='statsWrapper left-align'>
-        <p>{props.card.name}</p>
-        <p>Stats</p>
-        <ul>
-          <li>Home Runs: {props.card.info.hr}</li>
-          <li>Average: {props.card.info.avg}</li>
-          <li>Hits: {props.card.info.hits}</li>
-          <li>RBI: {props.card.info.rbi}</li>
-          <li>Stolen Bases: {props.card.info.sb}</li>
-        </ul>
+      <div className='cardFrontWrapper'>
+        <img className='circle' src={props.card.imageUrl}></img>
+        <div className='statsWrapper left-align'>
+          <p>{props.card.name}</p>
+          <ul>
+            <li>Home Runs: {props.card.info.hr}</li>
+            <li>Average: {props.card.info.avg}</li>
+            <li>Hits: {props.card.info.hits}</li>
+            <li>RBI: {props.card.info.rbi}</li>
+            <li>Stolen Bases: {props.card.info.sb}</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- server/data/baseballData.js
+++ server/data/baseballData.js
@@ -18,7 +18,8 @@ module.exports = {
       avg: 302,
       hits: 178,
       rbi: 91
-    }
+    },
+    imageUrl: 'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30193.png&w=350&h=254'
   },
   'Joey_Votto': {
     name: 'Joey Votto',
@@ -39,7 +40,8 @@ module.exports = {
       avg: 297,
       hits: 172,
       rbi: 95
-    }
+    },
+    imageUrl: 'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31027.png&w=350&h=254'
   },
   'David_Ortiz': {
     name: 'David Ortiz',
@@ -49,7 +51,8 @@ module.exports = {
       avg: 316,
       hits: 169,
       rbi: 127
-    }
+    },
+    imageUrl: 'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/3748.png&w=350&h=254'
   },
   'Nolan_Arenado': {
     name: 'Nolan Arenado',
@@ -59,7 +62,8 @@ module.exports = {
       avg: 294,
       hits: 182,
       rbi: 133
-    }
+    },
+    imageUrl: 'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31261.png&w=350&h=254'
   },
   'Jose_Altuve': {
     name: 'Jose Altuve',
@@ -69,7 +73,8 @@ module.exports = {
       avg: 338,
       hits: 216,
       rbi: 96
-    }
+    },
+    imageUrl: 'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31662.png&w=350&h=254'
   },
   'Mookie_Betts': {
     name: 'Mookie Betts',
@@ -79,6 +84,7 @@ module.exports = {
       avg: 318,
       hits: 214,
       rbi: 113
-    }
+    },
+    imageUrl: 'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/33039.png&w=350&h=254'
   }
 };
