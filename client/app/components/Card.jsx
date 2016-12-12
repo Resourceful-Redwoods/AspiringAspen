0ebe64232a9d6c078fadd9f17d2edad97e0501b6
Bruce Paul <brucewpaul@gmail.com>
2016-12-12 13:18:57 -0800

    fix typo

--- client/app/components/Card.jsx
+++ client/app/components/Card.jsx
@@ -6,17 +6,17 @@ const Card = (props) => {
 
 
   return (
-    <div className={'cardFront hoverable center-block' + (this.state.isSelected ? 'selected' : '')} onClick={() => this.props.selectCard(this.props.card)}>
+    <div className='cardFront hoverable center-block' onClick={() => props.selectCard(props.card)}>
       <div className='cardFrontWrapper'>
-        <img className='circle' src={this.props.card.imageUrl}></img>
+        <img className='circle' src={props.card.imageUrl}></img>
         <div className='statsWrapper left-align'>
-          <p>{this.props.card.name}</p>
+          <p>{props.card.name}</p>
           <ul>
-            <li>Home Runs: {this.props.card.info['Home Runs']}</li>
-            <li>Average: {this.props.card.info['Average']}</li>
-            <li>Hits: {this.props.card.info['Hits']}</li>
-            <li>RBI: {this.props.card.info['RBI']}</li>
-            <li>Stolen Bases: {this.props.card.info['Stolen Bases']}</li>
+            <li>Home Runs: {props.card.info['Home Runs']}</li>
+            <li>Average: {props.card.info['Average']}</li>
+            <li>Hits: {props.card.info['Hits']}</li>
+            <li>RBI: {props.card.info['RBI']}</li>
+            <li>Stolen Bases: {props.card.info['Stolen Bases']}</li>
           </ul>
         </div>
       </div>
