d4ed202fe0d0a66ff5c525faab0778f73d26e11d
Mikel Duffy <mikelduffy1@gmail.com>
2016-12-14 10:17:52 -0800

    Continues implementing LeaderBoard

--- client/app/components/LeaderBoard.jsx
+++ client/app/components/LeaderBoard.jsx
@@ -2,9 +2,32 @@ import React, { PropTypes } from 'react';
 
 
 class LeaderBoard extends React.Component {
-  
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  componentDidMount() {
+    const el = ReactDOM.findDOMNode(this);
+    TweenMax.fromTo('.waiting', 0.7, {y: -100, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeOut});
+  }
+  componentWillUnmount() {
+    const el = ReactDOM.findDOMNode(this);
+    TweenMax.fromTo('.waiting', 0.7, {y: 0, opacity: 1}, {y: -100, opacity: 0, ease: Expo.easeOut});
+  }
+
+  render () {
+    return (
+    <div className='cover'>
+      <div className="waiting z-depth-5 valign-wrapper">
+        <div className='valign center-block'>
+          <p>Welcome {this.props.username}!</p>
+          <h2 className=''>SEARCHING</h2>
+          <button onClick={ this.props.cancelMatchmaking.bind(this) } className="playbtn"> Cancel </button>
+        </div>
+      </div>
+    </div>
+    );
   }
 }
 
