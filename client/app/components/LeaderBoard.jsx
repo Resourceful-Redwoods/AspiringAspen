228799cad9dac4f87540e6812230c1c3ff3fe937
Mikel Duffy <mikelduffy1@gmail.com>
2016-12-14 11:28:03 -0800

    Implements LeaderBoard

--- client/app/components/LeaderBoard.jsx
+++ client/app/components/LeaderBoard.jsx
@@ -4,13 +4,18 @@ import React, { PropTypes } from 'react';
 class LeaderBoard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      leaders: props.users.sort((a, b) => (a.wins / a.loses) < (b.wins / b.loses))
+                          .slice(0, 10)
+    };
   }
 
   componentDidMount() {
     const el = ReactDOM.findDOMNode(this);
     TweenMax.fromTo('.waiting', 0.7, {y: -100, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeOut});
   }
+
   componentWillUnmount() {
     const el = ReactDOM.findDOMNode(this);
     TweenMax.fromTo('.waiting', 0.7, {y: 0, opacity: 1}, {y: -100, opacity: 0, ease: Expo.easeOut});
@@ -19,16 +24,29 @@ class LeaderBoard extends React.Component {
   render () {
     return (
     <div className='cover'>
-      <div className="waiting z-depth-5 valign-wrapper">
-        <div className='valign center-block'>
-          <p>Welcome {this.props.username}!</p>
-          <h2 className=''>SEARCHING</h2>
-          <button onClick={ this.props.cancelMatchmaking.bind(this) } className="playbtn"> Cancel </button>
-        </div>
+      <div className='leaderboard z-depth-5'>
+        <h2 className=''>LEADER BOARD</h2>
+        <ul class="collection">
+          {this.state.leaders.map((leader, key) => {
+            return (
+              <li key={key} class="collection-item avatar">
+                <h1>{key + 1}. </h1>
+                <span class="title">{leader.username}</span>
+                <p class="secondary-content">Wins: {leader.wins}<br />
+                   Loses: {leader.loses}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
     );
   }
 }
+
+LeaderBoard.propTypes = {
+  users: PropTypes.array.isRequired
+};
 
 export default LeaderBoard;
